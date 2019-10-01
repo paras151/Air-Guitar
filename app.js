e5b1c6349dfa54e35007d291a672c61347014910
paras madan <parasmadan15@yahoo.com>
2019-10-01 11:23:08 +0530

    score threshold

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const modelParams = {
     imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
     maxNumBoxes: 1,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.79,    // confidence threshold for predictions.
+    scoreThreshold: 0.85,    // confidence threshold for predictions.
   }
   
   navigator.getUserMedia = navigator.getUserMedia;
